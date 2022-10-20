05033a1aeee75d6c764d5d25d419adf4011f7280
Caio Neves <nevescaioneves@gmail.com>
2022-10-20 06:12:44 -0300

    inject da page matricula

--- script/script.js
+++ script/script.js
@@ -18,3 +18,72 @@ function activeLink(){
 
 injectContent();
 activeLink();
+
+// ----------- FEITO POR BRUNO MELO -----------
+
+//*cont que faz a formatacao e envia para o terceiro passo = Bruno Melo=*//
+const formato = {
+    phone(value){
+        return value
+        .replace(/\D/g,'')
+
+        .replace(/(\d{2})(\d)/,'($1)$2')
+
+        .replace(/(\d{4})(\d)/,'$1-$2')
+
+        .replace(/(\d{4})-(\d)(\d{4})/,'$1$2-$3')
+
+        .replace(/(-\d{4})\d+?$/,'$1')
+    }
+}
+
+//*identifica tods os campos input e pecore com o For*//
+document.querySelectorAll('input').forEach(($input)=>{
+
+//*Armazena e formata o valo recebido paa pegar o que esta dentro de data-js*//
+    const field = $input.dataset.js
+
+    $input.addEventListener('input', (e) => {
+        //*Enviar o valor ja formatado para o input atualizado em cada interacao*//
+        e.target.value = formato[field](e.target.value)
+    },false)
+
+})
+
+
+   
+function questao(){
+    alert( "Sua dúvida foi encaminhada com sucesso! Em breve entramos em contato!")   
+};
+
+// ----------- FEITO POR BRUNO WILKE -----------
+
+let ano = document.getElementById('anoInicio');
+let turma = document.getElementById('turma');
+let anos = [2022,2023,2024,2025];
+let turmas = [
+    "Infantil",
+    "1º Ano Fundamental",
+    "2º Ano Fundamental",
+    "3º Ano Fundamental",
+    "4º Ano Fundamental",
+    "5º Ano Fundamental",
+    "6º Ano Fundamental",
+    "7º Ano Fundamental",
+    "8º Ano Fundamental",
+    "9º Ano Fundamental",
+    "1ª Série Ensino Médio",
+    "2ª Série Ensino Médio",
+    "Terceirão"]
+
+window.addEventListener('load', function(){
+    criarOptions(ano,anos);
+    criarOptions(turma, turmas);
+})
+function criarOptions(idSelect, dados){
+    let options = '<option value="">---</option>'
+    for (const key in dados) {
+        options += `<option value="${key}">${dados[key]}</option>`;
+    } 
+    idSelect.innerHTML = options;
+}
