1fd57257f1ba3d266312471061fa0850ff01bfb9
Caio Neves <nevescaioneves@gmail.com>
2022-10-21 15:38:06 -0300

    finalizando detalhes

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@ async function injectContent(){
 
     let url = location.href; 
     file = url.split('=');
-    let view = (file[1] != undefined) ? file[1] : file[1] = 'home';
+    let view = (file[1] != undefined) ? file[1] : file[1] = 'home'; 
     const resp = await fetch (`pages/${file[1]}.html`);
     const html = await resp.text();
     let inject = document.getElementById('content');
